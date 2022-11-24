af45977d5381105e0b5a9fbbf97928d6f12cdbd2
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-24 11:14:57 +0900

    feat: 테이블에 맞게 엔티티의 id 데이터 타입을 Integer 로 변경 및 리포지토리에 반영

--- src/main/java/com/springboot/relationship/repository/HospitalRepository.java
+++ src/main/java/com/springboot/relationship/repository/HospitalRepository.java
@@ -3,6 +3,6 @@ package com.springboot.relationship.repository;
 import com.springboot.relationship.domain.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
 
 }
--- src/main/java/com/springboot/relationship/repository/ReviewRepository.java
+++ src/main/java/com/springboot/relationship/repository/ReviewRepository.java
@@ -3,5 +3,5 @@ package com.springboot.relationship.repository;
 import com.springboot.relationship.domain.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
 }
